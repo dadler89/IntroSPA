461e173e2358afb0a28e49f74aef86eb94db156c
Alex Adler <69403909+dadler89@users.noreply.github.com>
2021-08-11 12:15:15 -0500

    smol changes

--- components/views/Home.js
+++ components/views/Home.js
@@ -8,12 +8,12 @@ const kelvinToFahrenheit = (kelvinTemp) =>
 
 export default (st) => html`
   <section id="jumbotron">
-    <h2>Savvy Coders Jan. 2020 Cohort</h2>
-  
+    <h2>Savvy Coders Example Single Page Application</h2>
+
     <a href="" onClick="alert('Hello! You clicked the Button!')">"Call to Action" "Button"</a>
   </section>
-  
-  
+
+
   <h3 id="weather">
     The weather outside is weather...but the
     Temperature in ${st.weather.city} is
@@ -21,6 +21,6 @@ export default (st) => html`
     ${kelvinToFahrenheit(st.weather.feelsLike)}F. Humidity is at
     ${st.weather.humidity}%, and the weather is ... ${st.weather.description}
   </h3>
-  
+
   <img class="cover-img" src="${tarasenkoImg}" />
 `;
