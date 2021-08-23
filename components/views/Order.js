51910a36c826c49aeef5b84082b17f08d5c6397c
Alex Adler <69403909+dadler89@users.noreply.github.com>
2021-08-23 06:15:31 -0500

    Added toppings

--- components/views/Order.js
+++ components/views/Order.js
@@ -7,7 +7,7 @@ export default () => html`
     <label for="crust">Crust:</label>
     <select id="crust" name="crust">
       <option value="">Select a Crust</option>
-      <option value="thin">Thin</option>
+      <option value="thin">Thin af</option>
       <option value="chicago">Chicago</option>
       <option value="deep-dish">Deep Dish</option>
       <option value="hella-thick">Thick</option>
@@ -26,13 +26,14 @@ export default () => html`
     <label for="toppings">Toppings:</label>
     <input type="checkbox" id="id_of_checkbox1" class="items1" name="toppings" value="Chicken">
     <label for="top1">chicken</label>
-    <input type="checkbox" id="id_of_checkbox2" class="items1" name="toppings" value="Onion">
+    <input type="checkbox" id="id_of_checkbox2" class="items1" name="toppings" value="Bacon">
     <label for="top2">onion</label>
     <input type="checkbox" id="id_of_checkbox3" class="items1" name="toppings" value="Spinach">
     <label for="top3">spinach</label>
     <input type="checkbox" id="id_of_checkbox4" class="items1" name="toppings" value="Extra cheese">
     <label for="top4">extra cheese</label>
-    <input type="checkbox" id="id_of_checkbox5" class="items1" name="toppings" value="Red Pepper">
+    <input type="checkbox" id="id_of_checkbox5" class="items1" name="toppings" value="Sauage">
+    <input type="checkbox" id="id_of_checkbox6" class="items1" name="toppings" value="Pepperoni">
     <label for="top5">red pepper</label>
   </div>
   <input type="submit" name="submit" value="Submit Pizza">
